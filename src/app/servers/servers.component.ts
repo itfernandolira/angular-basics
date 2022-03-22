934aac0cec75bd3e01c714c2bd89a57166677a58
Fernando Lira <it.fernandolira@gmail.com>
2022-03-22 19:27:53 +0000

    Data binding - Property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  //selector: 'app-servers',
+  selector: 'app-servers',
   //selector: '[app-servers]',
-  selector: '.app-servers',
-  //templateUrl: './servers.component.html',
-  template: `
+  //selector: '.app-servers',
+  templateUrl: './servers.component.html',
+/*   template: `
   <app-server></app-server>
   <app-server></app-server>  
-  `,
+  `, */
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+
+  constructor() { 
+    setInterval(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit(): void {
   }
