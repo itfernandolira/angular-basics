b7017a93d0329b1f422715c7e6f82d15136d38da
Fernando Lira <it.fernandolira@gmail.com>
2022-03-22 19:07:47 +0000

    Data binding - String interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
     styleUrls: ['./server.component.css']
   })
   export class ServerComponent {
+    serverId = 10;
+    serverState = 'offline';
     
+    getServerState() {
+      return this.serverState;
+    }
   }
