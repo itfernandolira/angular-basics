188b4387536d92d4d745b6772c25f6d67a8efa79
Fernando Lira <it.fernandolira@gmail.com>
2022-03-23 12:33:53 +0000

    Data binding - Event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
+  serverCreationStatus = "No server was created!";
 
   constructor() { 
     setInterval(() => {
@@ -22,6 +23,10 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server created!";
   }
 
 }
